27cc7c0b09b7ed1654bfb397bd390ae3b30c8df8
unnirg <omarsajid434@gmail.com>
2020-12-28 13:29:59 +0530

    updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Just created the python file
+What this does:
+	convert images in a directory to RGB then resize and rotate
+use:
+	python file image_directory new_directory
--- noobimg.py
+++ noobimg.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
-print("Simple image manipulation")
+
+import sys
+import os
+from PIL import Image
+new_dir = sys.argv[2]
+try:
+    directory = sys.argv[1]
+    print('Processing images in ' + directory)
+    for root ,dirs ,files in os.walk(directory,topdown=False):
+        for name in files:
+            path = os.path.join(root,name)
+            try:
+                im = Image.open(path)
+                if im.format == 'TIFF':
+                    img=im.convert('RGB').rotate(90).resize((128,128))
+                    new_path=os.path.join(new_dir,name)
+                    print(new_path)
+                    img.save(new_path,'JPEG')
+            except Exception as e:
+                print(e)
+
+except Exception as e:
+    print(e)
